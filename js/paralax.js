781300c8d951fc13d27c30526e05a76fe7771a6c
nixi-awoo <archer@localhost.localdomain>
2015-06-01 00:20:07 +0200

    Added footer & tweaked the styles

--- js/paralax.js
+++ js/paralax.js
@@ -1,7 +1,9 @@
 var header = document.getElementById("header");
+var footer = document.getElementById("footer");
 var speed = 0.5;
 
 window.onscroll = function() {
   var yOffset = window.pageYOffset;
   header.style.backgroundPosition = "0px "+ (yOffset * speed) + "px";
+  footer.style.backgroundPosition = "0px "+ (yOffset * speed) + "px";
 }
